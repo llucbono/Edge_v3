2a62b4d4a82b81171b3a8a6328c5ca060c1a8c15
Lluc Bono <llucbono@MacBook-Air-de-Lluc.local>
2021-08-23 15:56:24 +0200

    add json demos

---
+++ edge_comp/tests/test_payload.py
@@ -0,0 +1,20 @@
+
+from rest_framework.test import APITestCase
+
+
+
+
+class PayloadsTests(APITestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def test_fracaso(self):
+        assert False
+
+
+
+
+
